f4ac6e629ad9b78b9e16c39987f2f1ee6851c5c0
nicumicle <20170987+nicumicleI@users.noreply.github.com>
2024-11-16 19:20:49 +0100

    Change url and baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: '/assets/favicons/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://simple-jwt-login.github.io/website/',
+  url: 'https://simple-jwt-login.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/website',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
